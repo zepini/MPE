1c8d2f8eb39d5f6536cf2979f8fcaabdca38d2f8
zepini <j.domingues@ua.pt>
2017-10-12 15:22:58 +0100

    TPC1 feito

--- TPC1/TPC1.m
+++ TPC1/TPC1.m
@@ -11,18 +11,44 @@ axis([-2 2 -2 2])
 
 title('Espaço amostral');
 
+a = 2;
+b = 1;
+
 figure(2)
 condC = (x.^2 + y.^2 <= sqrt(sqrt(2)));
 plot(x(condC),y(condC),'y.');
 axis([-2 2 -2 2])
 pC = sum(condC)/N
 
-
 figure(3)
-condE = ((x/2).^2+y.^2 <= 1);
+condE = ((x/a).^2+(y/b).^2 <= 1);
 plot(x(condE),y(condE),'r.');
 axis([-2 2 -2 2])
 pE = sum(condE)/N
 
- = cumsum(condM)./(1:N);
-plot(1:N,PM);
+figure(4)
+pCC = cumsum(condC)./(1:N);
+plot(1:N,pCC);
+
+figure(5)
+pEE = cumsum(condE)./(1:N);
+plot(1:N,pEE);
+
+AreaC = pi*sqrt(sqrt(2));
+
+AreaE = pi*a*b;
+
+Total = (2-(-2))*(2-(-2));
+
+pCanalitica = AreaC/Total
+pEanalitica = AreaE/Total
+
+
+
+
+
+
+
+
+
+
